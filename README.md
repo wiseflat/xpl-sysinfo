c90b9e46265884efccc2f76822c137c7543543dc
Mathieu Garcia <mgarcia@wiseflat.com>
2014-12-16 19:02:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+xpl-sysinfo
+==========
+
+Node JS layer to send system info through xPL
---
+++ index.js
@@ -0,0 +1,19 @@
+var xpllirc = require("./lib/xpl-sysinfo");
+
+var wt = new xpllirc(null, {
+	xplSource: 'bnz-sysinfo.wiseflat'
+});
+
+wt._init(function(error, xpl) {
+
+	if (error) {
+		console.error(error);
+		return;
+	}
+        
+        setInterval(function(){
+                wt.send();
+        }, 30 * 1000); 
+        
+});
+
