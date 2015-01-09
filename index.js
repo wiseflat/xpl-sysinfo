2f50007fed1ab99609b77a18ffd89e07e297193d
Mathieu Garcia <mgarcia@wiseflat.com>
2015-01-09 01:04:51 +0100

    correction d'une boulette

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ wt._init(function(error, xpl) {
 		return;
 	}
          
+        setInterval(function(){
+                wt.send_os_cpuFree();
+                wt.send_os_cpuUsage();
+                wt.send_os_freememPercentage();
+        }, 30 * 1000);
 });
 
