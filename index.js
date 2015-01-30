18388e476089558d0849ca40b11083cdce0dd4ca
math <math@mgarcia>
2015-01-30 17:09:49 +0100

    Mise à jour du programme

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var xplsuncalc = require("./lib/xpl-sysinfo");
+var xplsysinfo = require("./lib/xpl-sysinfo");
 
-var wt = new xplsuncalc(null, {
+var wt = new xplsysinfo(null, {
 	//xplSource: 'bnz-sysinfo.wiseflat'
 });
 
@@ -12,9 +12,18 @@ wt._init(function(error, xpl) {
 	}
          
         setInterval(function(){
-                wt.send_os_cpuFree();
-                wt.send_os_cpuUsage();
-                wt.send_os_freememPercentage();
+                wt.send_platform();
+                wt.send_cpuCount();
+                wt.send_sysUptime();
+                wt.send_processUptime();
+                wt.send_freemem();
+                wt.send_totalmem();
+                wt.send_freememPercentage();
+                wt.send_harddrive();
+                wt.send_allLoadavg();
+                wt.send_cpuFree();
+                wt.send_cpuUsage();
+                //wt.send_getCPUInfo();
         }, 30 * 1000);
 });
 
