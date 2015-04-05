91af1d663e992801e20793e78c0bd54c3877cb49
Mathieu Garcia <mgarcia@wiseflat.com>
2015-04-05 16:44:03 +0200

    evo - add version from package.json

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var xplsysinfo = require("./lib/xpl-sysinfo");
+var schema_sysinfobasic = require('/etc/wiseflat/schemas/sysinfo.basic.json');
+var schema_sysinfoconfig = require('/etc/wiseflat/schemas/sysinfo.config.json');
 
 var wt = new xplsysinfo(null, {
-	//xplSource: 'bnz-sysinfo.wiseflat'
+	xplLog: false,
+        forceBodySchemaValidation: false
 });
 
 wt._init(function(error, xpl) {
@@ -10,7 +13,18 @@ wt._init(function(error, xpl) {
 		console.error(error);
 		return;
 	}
-         
+
+	xpl.addBodySchema(schema_sysinfobasic.id, schema_sysinfobasic.definitions.body);
+	xpl.addBodySchema(schema_sysinfoconfig.id, schema_sysinfoconfig.definitions.body);
+	
+        // Load config file into hash
+        wt.readConfig();
+        
+        // Send every minutes an xPL status message 
+        setInterval(function(){
+                wt.sendConfig();
+        }, 60 * 1000);
+	
         setInterval(function(){
                 wt.send_platform();
                 wt.send_cpuCount();
